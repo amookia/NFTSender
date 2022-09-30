35f60ff61cea1fa9c6f869db6f643d984e1a8592
amookia <kiasn21@gmail.com>
2022-09-30 21:07:55 +0330

    count to 5

--- getgems/vars.py
+++ getgems/vars.py
@@ -4,6 +4,6 @@ g_guns = {
     "query": "{\"$and\":[{\"collectionAddress\":\"EQBpOQjo6uIpkH-GqJ1oObqVjyATQEJ1PnIrM_52f3nSE_rb\"}]}",
     "attributes": "[[\"Elements\",[\"Diamond\",null,\"Common\",\"Rare\",\"Gold\"]],[\"Inner Body\",[\"Rare\"]],[\"Outer Body\",[\"Rare\",\"Gold\"]],[\"Type\",[\"G-Pistols\",\"G-Shotguns\"]]]",
     "sort": "[{\"isOnSale\":{\"order\":\"desc\"}},{\"price\":{\"order\":\"asc\"}},{\"index\":{\"order\":\"asc\"}}]",
-    "count": 30
+    "count": 5
     }
 #------------------------
