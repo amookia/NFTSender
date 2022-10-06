061cee72354dec5ab654492882694ca52e5ddbec
amookia <kiasn21@gmail.com>
2022-10-06 23:16:04 +0330

    show attrs for ton diamonds

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     while True:
         Thread(sendToApi(vars.g_guns,'G-Guns')).start()
         Thread(sendToApi(vars.MetaForest_Bunnies,'MetaForest_Bunnies')).start()
-        Thread(sendToApi(vars.TON_Diamonds,'TON Diamonds')).start()
+        Thread(sendToApi(vars.TON_Diamonds,'TON Diamonds','Diamonds')).start()
         Thread(sendToApi(vars.Tonlanders_Sheeps,'Tonlanders - Sheeps')).start()
         Thread(sendToApi(vars.Ton_Frogs,'Ton Frogs')).start()
 
--- test.py
+++ test.py
@@ -2,5 +2,5 @@ from getgems import vars
 from getgems.checker import nftSearch
 
 
-x = nftSearch(vars.TON_Diamonds,"",attr="")
+x = nftSearch(vars.TON_Diamonds,"",attr="Diamonds")
 print(x)
