f3016c2f2bf9a05d481a3e27eb1f24d82dea2f6c
amookia <kiasn21@gmail.com>
2022-10-09 23:22:32 +0330

    domain back

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import time
 
 def sendToApi(var,c):
     try:
-        requests.post('https://tomantoncoin.com/nft/connect.php',json=checker.nftSearch(var,c))
+        requests.post('http://ashams.tk/nftchannel/connect.php',json=checker.nftSearch(var,c))
     except Exception as e:
         print(e)
 
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     while True:
         Thread(sendToApi(vars.g_guns,'G-Guns')).start()
         Thread(sendToApi(vars.MetaForest_Bunnies,'MetaForest_Bunnies')).start()
-        Thread(sendToApi(vars.TON_Diamonds,'TON Diamonds','Diamonds')).start()
+        Thread(sendToApi(vars.TON_Diamonds,'TON Diamonds')).start()
         Thread(sendToApi(vars.Tonlanders_Sheeps,'Tonlanders - Sheeps')).start()
         Thread(sendToApi(vars.Ton_Frogs,'Ton Frogs')).start()
 
