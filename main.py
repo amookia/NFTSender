527f35e848175188c3da9444d2535946e784419e
amookia <kiasn21@gmail.com>
2022-11-08 22:54:03 +0330

    Revert "schedule"
    This reverts commit e4e918b0777a6977631b6c25df0a2bb51103fb59.

--- main.py
+++ main.py
@@ -1,48 +1,36 @@
 from getgems import checker,vars
 import requests
 from threading import Thread
-import schedule
 import time
 
 
-
-def GGuns():
+def sendToApi(var,c):
     try:
-        requests.post('https://tomantoncoin.com/nft/connect.php',json=checker.nftSearch(vars.g_guns,'G-Guns'))
-    except:
-        pass
-
-def MetaForest():
-    try:
-        requests.post('https://tomantoncoin.com/nft/connect.php',json=checker.nftSearch(vars.MetaForest_Bunnies,'MetaForest_Bunnies'))
-    except:
-        pass
-
-def TonDiamonds():
-    try:
-        requests.post('https://tomantoncoin.com/nft/connect.php',json=checker.nftSearch(vars.TON_Diamonds,'TON Diamonds'))
-    except:
-        pass
-
-def Tonlanders():
-    try:
-        requests.post('https://tomantoncoin.com/nft/connect.php',json=checker.nftSearch(vars.Tonlanders_Sheeps,'Tonlanders - Sheeps'))
-    except:
-        pass
-
-def TonFrogs():
-    try:
-        requests.post('https://tomantoncoin.com/nft/connect.php',json=checker.nftSearch(vars.Ton_Frogs,'Ton Frogs'))
+        requests.post('https://tomantoncoin.com/nft/connect.php',json=checker.nftSearch(var,c))
     except:
         pass
 
 if __name__ == '__main__':
-    schedule.every(10).seconds.do(GGuns)
-    schedule.every(10).seconds.do(MetaForest)
-    schedule.every(10).seconds.do(TonDiamonds)
-    schedule.every(10).seconds.do(Tonlanders)
-    schedule.every(10).seconds.do(TonFrogs)
-
     while True:
-        schedule.run_pending()
-        time.sleep(1)
+        t1 = Thread(sendToApi(vars.g_guns,'G-Guns'))
+        t2 = Thread(sendToApi(vars.MetaForest_Bunnies,'MetaForest_Bunnies'))
+        t3 = Thread(sendToApi(vars.TON_Diamonds,'TON Diamonds'))
+        t4 = Thread(sendToApi(vars.Tonlanders_Sheeps,'Tonlanders - Sheeps'))
+        t5 = Thread(sendToApi(vars.Ton_Frogs,'Ton Frogs'))
+
+        t1.start()
+        t1.join()
+
+        t2.start()
+        t2.join()
+
+        t3.start()
+        t3.join()
+
+        t4.start()
+        t4.join()
+
+        t5.start()
+        t5.join()
+
+        time.sleep(15)
