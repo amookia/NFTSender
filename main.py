572dd2f1d8ee351459c61cbf62114d363b37efc8
amookia <kiasn21@gmail.com>
2022-11-20 01:20:50 +0330

    changes

--- main.py
+++ main.py
@@ -7,29 +7,59 @@ from getgems.history import historyCollectionNftItems
 from getgems.checker import getNftByAddress
 
 def GGuns():
-  addresses = historyCollectionNftItems('EQBpOQjo6uIpkH-GqJ1oObqVjyATQEJ1PnIrM_52f3nSE_rb')
-  for item in addresses:
-    requests.post('https://tomantoncoin.com/nft/connect.php',json=(getNftByAddress(item,'G-Guns')))
+    addresses = historyCollectionNftItems('EQBpOQjo6uIpkH-GqJ1oObqVjyATQEJ1PnIrM_52f3nSE_rb')
+    count = 0
+    arr = []
+    for item in addresses:
+        if count < 5:
+            arr.append(getNftByAddress(item,'G-Guns'))
+        count +=1
+    requests.post('https://tomantoncoin.com/nft/connect.php',json=(arr))
+  
 
 def MetaForest():
     addresses = historyCollectionNftItems('EQAu3uEK8pxgAZ29QSYhRtDwmQpqJRbziH24gnud8BDGLRB6')
+    count = 0
+    arr = []
     for item in addresses:
-        requests.post('https://tomantoncoin.com/nft/connect.php',json=(getNftByAddress(item,'MetaForest_Bunnies')))
+        if count < 5:
+            arr.append(getNftByAddress(item,'MetaForest_Bunnies'))
+    count +=1
+    requests.post('https://tomantoncoin.com/nft/connect.php',json=(arr))
+    
 
 def TonDiamonds():
     addresses = historyCollectionNftItems('EQAG2BH0JlmFkbMrLEnyn2bIITaOSssd4WdisE4BdFMkZbir')
+    count = 0
+    arr = []
     for item in addresses:
-        requests.post('https://tomantoncoin.com/nft/connect.php',json=(getNftByAddress(item,'TON Diamonds')))
+        if count < 5:
+            arr.append(getNftByAddress(item,'TON Diamonds'))
+    count +=1
+    requests.post('https://tomantoncoin.com/nft/connect.php',json=(arr))
+
 
 def Tonlanders():
     addresses = historyCollectionNftItems('EQDi0t0R8yjV1Yu8lWHkDawz2xihHqsGB61-bQVf7EQ8SteO')
+    count = 0
+    arr = []
     for item in addresses:
-        requests.post('https://tomantoncoin.com/nft/connect.php',json=(getNftByAddress(item,'Tonlanders - Sheeps')))
+        if count < 5:
+            arr.append(getNftByAddress(item,'Tonlanders - Sheeps'))
+    count +=1
+    requests.post('https://tomantoncoin.com/nft/connect.php',json=(arr))
+   
 
 def TonFrogs():
     addresses = historyCollectionNftItems('EQAu3uEK8pxgAZ29QSYhRtDwmQpqJRbziH24gnud8BDGLRB6')
+    count = 0
+    arr = []
     for item in addresses:
-        requests.post('https://tomantoncoin.com/nft/connect.php',json=(getNftByAddress(item,'Ton Frogs')))
+        if count < 5:
+            arr.append(getNftByAddress(item,'Ton Frogs'))
+    count +=1
+    requests.post('https://tomantoncoin.com/nft/connect.php',json=(arr))
+
 
 def CheckErr():
     r = requests.post('https://api.getgems.io/graphql')
